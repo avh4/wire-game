5c953ff387aa6389b083b74d4f228fe379a09369
Aaron VonderHaar <gruen0aermel@gmail.com>
2015-01-19 21:04:08 -0800

    Draw nodes

--- src/Main.elm
+++ src/Main.elm
@@ -21,10 +21,14 @@ init =
 
 background w h = [ rect (toFloat w) (toFloat h) |> filled darkOrange ]
 
+node w h (x,y) = oval 35 35 |> filled darkBrown |> move (((x-0.5) * (toFloat w)), ((y-0.5) * (toFloat h)))
+
+nodes w h points = Array.map (node w h) points
 
 render : (Int,Int) -> Model -> Element
 render (w,h) m =
   [ background w h
+  , nodes w h m.points |> Array.toList
   ]
   |> List.concat
   |> collage w h
