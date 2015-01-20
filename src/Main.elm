e43e16f8f01d2972fd99f2e33fc42d4b8aee5ebe
Aaron VonderHaar <gruen0aermel@gmail.com>
2015-01-19 20:55:30 -0800

    Draw background

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,32 @@
 module Main where
 
-import Text
+import List
+import Array
+import Color (..)
+import Graphics.Element (..)
+import Graphics.Collage (..)
 
-main = Text.asText "Hello world"
+type alias Edge = (Int, Int)
+type alias Point = (Float, Float)
+type alias Model =
+  { edges: List Edge
+  , points: Array.Array Point
+  }
+
+init : Model
+init =
+  { edges= [(0,1), (0,2), (2,1)]
+  , points= Array.fromList [(0.5, 0.2), (0.3, 0.6), (0.8, 0.65)]
+  }
+
+background w h = [ rect (toFloat w) (toFloat h) |> filled darkOrange ]
+
+
+render : (Int,Int) -> Model -> Element
+render (w,h) m =
+  [ background w h
+  ]
+  |> List.concat
+  |> collage w h
+
+main = render (800,600) init
