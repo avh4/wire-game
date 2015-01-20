419a3057261e3bf91557bb7ce9c9e9ea235ba8d3
Aaron VonderHaar <gruen0aermel@gmail.com>
2015-01-19 21:31:52 -0800

    Highlight the selected node

--- src/Main.elm
+++ src/Main.elm
@@ -14,12 +14,14 @@ type alias Point = (Float, Float)
 type alias Model =
   { edges: List Edge
   , points: Array.Array Point
+  , selected: Maybe Int
   }
 
 init : Model
 init =
   { edges= [(0,1), (0,2), (2,1)]
   , points= Array.fromList [(0.5, 0.2), (0.3, 0.6), (0.8, 0.65)]
+  , selected= Just 0
   }
 
 background w h = [ rect (toFloat w) (toFloat h) |> filled darkOrange ]
@@ -27,8 +29,8 @@ background w h = [ rect (toFloat w) (toFloat h) |> filled darkOrange ]
 conv : Int -> Float -> Float
 conv w x = (x-0.5) * (toFloat w)
 
-node w h (x,y) = oval 35 35
-  |> filled darkBrown
+node w h sel i (x,y) = oval 35 35
+  |> filled (if sel == (Just i) then lightBrown else darkBrown)
   |> move (conv w x, conv h y)
 
 lookupPoint points p = case Array.get p points of
@@ -48,12 +50,12 @@ drawEdge w h ((x1,y1),(x2,y2)) =
 render : (Int,Int) -> Model -> Element
 render (w,h) m =
   [ background w h
-  , m.points
-    |> Array.map (node w h)
-    |> Array.toList
   , m.edges
     |> List.map (lookupEdge m.points)
     |> List.map (drawEdge w h)
+  , m.points
+    |> Array.indexedMap (node w h m.selected)
+    |> Array.toList
   ]
   |> List.concat
   |> collage w h
